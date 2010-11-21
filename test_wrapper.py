3a9c495d070d29cf85cf895154fba16b492cd20f
Scott Carr <scott@localhost.localdomain>
2010-11-20 21:31:45 -0500

    improved efficiency of test_wrapper.p

--- test_wrapper.py
+++ test_wrapper.py
@@ -8,31 +8,34 @@ PERCENT_TEST = .20
 
 #need to chop up affinities.dump
 
-test = []
-training = []
-
+length = 0
+#we want to know how big the file is
 with open("publicvotes-20101018_affinities.dump") as f:
 	for line in f:
-		training.append(line)
+		length += 1 
 
-test_set_size = int(len(training)*PERCENT_TEST)
+test_set_size = int(length*PERCENT_TEST)
+test_set_idexes = random.sample(range(length), test_set_size)
+test_set_dict = {}
+for index in test_set_idexes:
+	test_set_dict[index] = 1
 
-print "Done reading file"
-
-#pdb.set_trace()
-
-for i in range(test_set_size):
-	if i % 100 == 0:
-		print i,"of,",test_set_size
-	selected = random.choice(training)
-	test.append(selected)
-	training.remove(selected)
+training_set = []
+test_set = []
+i = 0
+with open("publicvotes-20101018_affinities.dump") as f:
+	for line in f:
+		if i in test_set_dict:
+			test_set.append(line)		
+		else:
+			training_set.append(line)
+		i += 1
 
 with open("test_dataset",'w') as f:
-	for line in test:
+	for line in test_set:
 		f.write(line)
 
 with open("training_dataset",'w') as f:
-	for line in training:
+	for line in training_set:
 		f.write(line)
 
